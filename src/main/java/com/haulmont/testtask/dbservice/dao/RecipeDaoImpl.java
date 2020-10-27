198cafeac0a6be45861ca464e3ec0a0fe0304a8f
Eugene Nikonov <nikonovse96@gmail.com>
2020-10-27 16:10:43 +0400

    fixed imports

--- src/main/java/com/haulmont/testtask/dbservice/dao/RecipeDaoImpl.java
+++ src/main/java/com/haulmont/testtask/dbservice/dao/RecipeDaoImpl.java
@@ -10,11 +10,9 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 public class RecipeDaoImpl implements RecipeDao {
